d0940ea1b91c7475c8eccfb1285423df950dbdaf
LeonardoC-Jesus <dev.leonardocdejesus@gmail.com>
2026-07-08 15:10:49 -0300

    feat: cria exceção InvalidFormatException

---
+++ src/main/java/br/leonardocdejesus/calculadora_imc/exception/InvalidFormatException.java
@@ -0,0 +1,7 @@
+package br.leonardocdejesus.calculadora_imc.exception;
+
+public class InvalidFormatException extends RuntimeException {
+    public InvalidFormatException(String message) {
+        super(message);
+    }
+}
