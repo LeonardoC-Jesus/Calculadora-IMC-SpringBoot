b227272c2dd5a1947da35e0eeafb04e3f2e7f314
LeonardoC-Jesus <dev.leonardocdejesus@gmail.com>
2026-07-08 16:00:56 -0300

    feat: cria a classe ImcRegistroController

---
+++ src/main/java/br/leonardocdejesus/calculadora_imc/controller/ImcRegistroController.java
@@ -0,0 +1,31 @@
+package br.leonardocdejesus.calculadora_imc.controller;
+
+import br.leonardocdejesus.calculadora_imc.dto.ImcRegistroDto;
+import br.leonardocdejesus.calculadora_imc.model.ImcRegistro;
+import br.leonardocdejesus.calculadora_imc.service.ImcRegistroService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/v1/imc")
+public class ImcRegistroController {
+
+    private final ImcRegistroService imcRegistroService;
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ImcRegistro> getImcRegistro() {
+        return imcRegistroService.retornarHistoricoDeImc();
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public double postImcRegistro(@RequestBody ImcRegistroDto imcRegistroDto) {
+        return imcRegistroService.registrarImc(imcRegistroDto);
+    }
+}
